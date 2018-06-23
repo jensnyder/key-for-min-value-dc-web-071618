0ef16f12c5db5c2168f2f136c3d69386d14e6332
Jennifer Snyder <jlsnyder09@gmail.com>
2018-06-23 12:49:13 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,12 +3,13 @@
 
 def key_for_min_value(name_hash)
   min_value = nil
+  min_key = nil
   name_hash.each do |key, value|
     if min_value == nil
-      key
+      min_key = key
     elsif value < min_value
-      key
+      min_key = key
     end
   end
-  key
+  min_key = key
 end
